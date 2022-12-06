37f45152000121d8bf06f0d6a6c30a6cf5d01a82
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-12-06 12:24:09 +0500

    day 6 solutions added

---
+++ day6_part2/day6.py
@@ -0,0 +1,15 @@
+with open("ip.txt", "r") as f:
+    data_stream = f.readline().strip()
+
+start = 0
+chars = list()
+chars.append(data_stream[0])
+for end in range(1, len(data_stream)):
+    chars.append(data_stream[end])
+    if ((end - start) + 1) == 14:  # 4 len window made
+        if len(set(chars)) == len(chars):  # all in window unique
+            print(end + 1)
+            exit()
+        start += 1
+        chars.pop(0)
+    
