60fb45db1c4aece35a729ab3ecebbdef972a5953
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-12-07 18:13:13 +0500

    Day 7 solutions added

---
+++ day7_part2/day7.py
@@ -0,0 +1,76 @@
+class Folder:
+    def __init__(self, folder_name, parent_folder) -> None:
+        self.folder_name = folder_name
+        self.parent_folder = parent_folder
+        self.folders = []
+        self.files_size = 0
+
+    def get_size(self):
+        total_size = 0
+        total_size += self.files_size
+        return total_size
+
+    def add_child_folder(self, folder):
+        self.folders.append(folder)
+
+    def increment_size(self, increment):
+        self.files_size += increment
+        if self.parent_folder:
+            self.parent_folder.increment_size(increment)
+
+    def __repr__(self) -> str:
+        return f"<Folder- {self.folder_name}, Childeren-{[f.folder_name for f in self.folders]}>"
+
+home_folder = Folder(folder_name="/", parent_folder=None)
+folders = set()
+folders.add(home_folder)
+folders_stack = [home_folder]
+with open("ip.txt", "r") as f:
+    f.readline()  # skip 1st line
+    for line in f:
+        line = line.strip()
+        current_folder = folders_stack[-1]
+
+        # no processing needed on line for list command
+        if "$ ls" in line:
+            continue
+
+        # if directory changed
+        elif "$ cd" in line:
+            cd_folder = line[ line.find("$ cd") + 5 : ].strip()
+            # if cd_folder == "/":
+            #     folders_stack = [home_folder]
+            if cd_folder == "..":
+                # if len(folders_stack) > 1:
+                folders_stack.pop()
+            else:
+                for folder in folders:
+                    if (folder.folder_name == cd_folder) and (folder.parent_folder == current_folder):
+                        folders_stack.append(folder)
+                        break
+
+    
+        # a file or folder info line
+        else:
+
+            if "dir" in line:  # is a folder
+                new_child_folder = Folder(folder_name=line[ line.find("dir") + 4 : ].strip(), parent_folder=current_folder)
+                folders.add(new_child_folder)
+                current_folder.add_child_folder(folder=new_child_folder)
+
+            else:  # is a file
+                file_size = int( line[ : line.find(" ")])  # slice line till before first space
+                current_folder.increment_size(file_size)
+
+min_delete_size = float('inf')
+available_space = 70000000 - home_folder.get_size()
+required_space = 30000000 - available_space
+for folder in folders:
+    folder_size = folder.get_size()
+    if folder_size == required_space:
+        min_delete_size = folder_size
+        break
+    elif folder_size > required_space:
+        min_delete_size = min(folder_size, min_delete_size)
+
+print(min_delete_size)
